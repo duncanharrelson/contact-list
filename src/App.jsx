b895334dfb8fdf0a442d41e7672bf2fb7c8d3534
duncanharrelson <duncan.harrelson@gmail.com>
2023-09-07 10:28:35 -0500

    add ContactList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
 
+import ContactList from './components/ContactList'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App() {  
   return (
     <>
       
-      <div className="card">
-       
-      </div>
+      <ContactList />
      
     </>
   )
